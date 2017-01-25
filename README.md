148183d5351a3f5285b22968e40d8a05ac301500
ndruyang <ndru.yang@gmail.com>
2017-01-24 20:10:03 -0800

    Incorporated Calibration for Phrases

--- README.md
+++ README.md
@@ -4,8 +4,18 @@
 Assumes the signal detection task is strctured as a series of blocks of n trials at a given difficulty level.
 
 How to use:
- - `y`: correct response
- - `n`: incorrect response
+ - first indicate whether you are calibrating words or phrases
+
+ - if words:
+   - `y`: correct response
+   - `n`: incorrect response
+ 
+ - if phrases:
+   - enter the number of lexical categories in the phrase that the subject correctly
+   	 indentifies
+   - then enter the path to .csv file
+
+ - for both:
  - `new`: initiate new block
  - `export`: export settings as .csv  
  	- includes info about: participant ID, noise levels, signal levels, and actuator placement
--- calibrationassist.py
+++ calibrationassist.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import os
 import csv
+import random
 
 #----------------------------------------------------------------------
 csvpath = 'calibrationLogs.csv'
@@ -13,19 +14,124 @@ consoleSys = 'unix' #set to 'windows' or 'unix'
 # main function
 # returns: nothing
 def main():
-
-	clear = 'clear' 
-	correctResponses = 0
-	trialNumber = 0
 	calibrated = False #for main loop
-	blockNumber = 1 
-	participantID = 0
-
 	#check os for console formatting
 	if consoleSys == 'unix':
 		clear = 'cls'
 
 	#Intro setup
+	# determine whether use is calibrating words or phrases
+	correct_Entry = False
+	typeOfCal = raw_input("Are you calibrating words or phrases? \n")
+	while correct_Entry == False:
+		if typeOfCal == "Words" or typeOfCal == "words":
+			correct_Entry = True
+			# implement the set of words later
+			# run the calibration loop for words
+			main_words(calibrated)
+		elif typeOfCal == "Phrases" or typeOfCal == "phrases":
+			correct_Entry = True
+			csvFile = raw_input("Please enter the path to the file: \n")
+			phrases = readCSV_Phrases(csvFile)
+			# run the calibration loop for phrases
+			main_phrases(calibrated, phrases)
+		else:
+			typeOfCal = raw_input("Please enter 'words' or 'phrases': \n")
+
+#----------------------------------------------------------------------
+# main loop for phrases as a function
+#
+# Boolean, Dict
+# returns : None
+def main_phrases(calibrated, phrases):
+	clear = 'clear' 
+	correctResponses = 0
+	trialNumber = 0
+	blockNumber = 1 
+	participantID = 0
+
+	participantID = raw_input("participant ID: \n")
+	print "Callibrate accuracy to ~33%\n"
+	print("Input the number of lexical category items that the subject identifies correctly as the score")
+	for i in phrases:
+		trialNumber+=1
+		print("Phrase: " + i)
+		var = raw_input("Score: \n")
+		
+		# score
+		if var=="0" or var=="1" or var=="2" or var=="3":
+			os.system(clear) #refresh screen
+			correctResponses = int(var)/phrases[i]
+			accuracy = calcAverage(correctResponses,trialNumber)
+			print " _________\n| block "+str(blockNumber)+" |\n------------------------------------"
+			print accuracyFormatting(accuracy)
+			print "| correct: "+str(correctResponses)+"| trial n: "+str(trialNumber)+"\n"
+		
+		#new block
+		if var=="new":
+			os.system(clear)
+			correctResponses = 0
+			trialNumber = 0
+			blockNumber +=1
+			print " _________\n| block "+str(blockNumber)+" |\n------------------------------------\n\nNew block!"
+		
+		#exit program
+		if var == "done" or var == "exit":
+			exit()
+
+		#resets vars
+		if var == "reset":
+			os.system(clear)
+			trialNumber = 0
+			correctResponses = 0
+			accuracy = 0
+			print " _________\n| block "+str(blockNumber)+" |\n------------------------------------"
+			print("| accuracy: "+str(accuracy))
+			print "| correct: "+str(correctResponses)+"| trial n: "+str(trialNumber)+"\n"
+			print("block settings reset!")
+
+		#start again
+		if var == "nuclear option":
+			confirm = raw_input("Will completely wipe history, are you sure? (y/n): ")
+			if (confirm=="y"):
+				os.system(clear)
+				trialNumber = 0
+				correctResponses = 0
+				blockNumber = 0
+				accuracy = 0
+				print " _________\n| block "+str(blockNumber)+" |\n------------------------------------"
+				print("| accuracy: "+str(accuracy))
+				print "| correct: "+str(correctResponses)+"| trial n: "+str(trialNumber)+"\n"
+				print("Complete reset!")
+
+		#exports csv
+		if var == "export":
+			noiseLevels = raw_input("noise level: ")
+			signalLevels = raw_input("signal level: ")
+			placement = raw_input("actuator placement: ")
+			confirm = raw_input("confirm? (y/n): ")
+
+			if confirm == "y":
+				print "    writing to file: `"+csvpath+"`..."
+				writeCSV(participantID,noiseLevels,signalLevels,placement)
+				print "    Complete!"
+		
+		#user help
+		if var == "...":
+			print "    commands:\n        y: correct response\n        n: incorrect response\n        new: initiate new block\n        export: export .csv\n        reset: reset the current block (if you made a mistake)\n        nuclear option: complete reset (start again from block 1)\n        exit: exit program"
+
+#----------------------------------------------------------------------
+# main loop for words as a function
+#
+# Boolean
+# returns : None
+def main_words(calibrated):
+	clear = 'clear' 
+	correctResponses = 0
+	trialNumber = 0
+	blockNumber = 1 
+	participantID = 0
+
 	participantID = raw_input("participant ID: \n")
 	print "Callibrate accuracy to ~33%\n" 
 	var = raw_input("input 'y' for correct, 'n' for incorrect, and 'new' to initiate a new block\n>")
@@ -163,7 +269,30 @@ def accuracyFormatting(accuracy):
 		return "| accuracy: "+str(accuracy)
 
 #----------------------------------------------------------------------
+# read CSV file for phrases and store in a dictionary
+# order of phrases in the dictionary is randomized
+#
+# String csv path
+# returns: dictionary
+def readCSV_Phrases(csvPath):
+	# reads the csv and stores items in each row as a 
+	# list element in a list
+	listOfPhrases = readCSV(csvPath)
+	# get rid of the first element ['phrase', 'lexicalCount']
+	listOfPhrases.pop(0)
+	# randomize the order of the phrases
+	random.shuffle(listOfPhrases)
+	# find out how many phrases there are
+	numOfPhrases = len(listOfPhrases)
+	phrases = {}
+	# store each row as a key:value pair in a dictionary
+	for i in range(numOfPhrases):
+		phrases[listOfPhrases[i][0]] = int(listOfPhrases[i][1])
+	return phrases
+
+#----------------------------------------------------------------------
 
 
-main()	
+main()
+
 
